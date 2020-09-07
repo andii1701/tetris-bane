21ff66dff8a6fd2ce084bd7900994ff831fb4426
Andrew Jones <andrew.jones@rea-group.com>
2020-09-07 21:59:50 +1000

    Add a block to world

--- src/game.rs
+++ src/game.rs
@@ -26,8 +26,15 @@ pub struct Color {
     b: u8,
 }
 
+pub struct Block {
+    positions: Vec<Position>,
+    color: Color,
+}
+
 pub struct World {
+    pub block: Block,
     pub board: [[Option<Color>; BOARD_SIZE.x as usize]; BOARD_SIZE.y as usize],
+
     pub fall_rate_millis: u128, // elapsed ms before blocks drop to next row
 
     pub block_drop_clock: Instant,
@@ -35,44 +42,56 @@ pub struct World {
 
 pub fn initialise() -> World {
     let mut board = [[None; BOARD_SIZE.x as usize]; BOARD_SIZE.y as usize];
-    board[0][0] = Some(Color { r: 255, g: 0, b: 0 });
+    let mut starting_block = Block {
+        positions: vec![Position { y: 0, x: 0 }],
+        color: Color { r: 0, g: 255, b: 0 },
+    };
     World {
         board: board,
+        block: starting_block,
         fall_rate_millis: 500,
         block_drop_clock: Instant::now(),
     }
 }
 
 pub fn update(event: &Option<InputEvent>, world: &mut World) {
-    for y in 0..BOARD_SIZE.y {
-        for x in 0..BOARD_SIZE.x {
-            match world.board[y as usize][x as usize] {
-                Some(color) => {
-                    if let Some(event) = event {
-                        match event {
-                            InputEvent::Left => {
-                                //world.position = new_position(world.position.x - 1, world.position.y)
+    if world.block_drop_clock.elapsed().as_millis() > world.fall_rate_millis {
+        for mut position in world.block.positions.iter_mut() {
+            let new_position = Position {
+                y: position.y + 1,
+                x: position.x,
+            };
+            if can_move_here(new_position) {
+                // clear old block from board
+                world.board[position.y as usize][position.x as usize] = None;
+                world.board[new_position.y as usize][new_position.x as usize] =
+                    Some(world.block.color);
+            }
+            *position = new_position;
+        }
+        world.block_drop_clock = Instant::now();
+    }
+    /*
+        for y in 0..BOARD_SIZE.y {
+            for x in 0..BOARD_SIZE.x {
+                match world.board[y as usize][x as usize] {
+                    Some(color) => {
+                        if let Some(event) = event {
+                            match event {
+                                InputEvent::Left => {
+                                    //world.position = new_position(world.position.x - 1, world.position.y)
+                                }
+
+                                InputEvent::Right => {}
+                                _ => {}
                             }
-
-                            InputEvent::Right => {}
-                            _ => {}
                         }
                     }
-
-                    if world.block_drop_clock.elapsed().as_millis() > world.fall_rate_millis {
-                        let new_position = Position { y: y + 1, x: x };
-                        if can_move_here(new_position) {
-                            let tmp = world.board[y as usize][x as usize];
-                            world.board[y as usize][x as usize] = None;
-                            world.board[new_position.y as usize][new_position.x as usize] = tmp;
-                            world.block_drop_clock = Instant::now();
-                        }
-                    }
+                    None => {}
                 }
-                None => {}
             }
         }
-    }
+    */
 }
 //
 fn can_move_here(p: Position) -> bool {
